cfaa1a09f7d920d4e138f11e8d185eacf83cee71
Anold <godatbelecof@gmail.com>
2025-05-15 20:14:20 +0200

    update java app

--- src/main/java/com/vut/App.java
+++ src/main/java/com/vut/App.java
@@ -1,13 +1,13 @@
 package com.vut;
 
 /**
- * Hello world!
+ * Simple Java App!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Simple Java App!" );
     }
 }
